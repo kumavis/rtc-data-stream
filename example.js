01be03077f710f13c5bf78814f7ecb80e3ceec1e
aaron <a.aa.akio.aaron@gmail.com>
2014-09-10 20:47:32 -0700

    Update example.js

--- example.js
+++ example.js
@@ -2,21 +2,18 @@ var quickconnect = require('rtc-quickconnect')
 var duplexEmitter = require('duplex-emitter')
 var rtcDataStream = require('./index.js')
 
-var rtcConnection = quickconnect('http://rtc.io/switchboard/', {
-  ns: 'dctest',
-  data: true,
-  debug: true
-})
+quickconnect('http://rtc.io/switchboard', { room: 'rtc-data-stream-demo' })
+  .createDataChannel('demo')
+  .on('channel:opened:demo', function(peerId, channel) {
 
-rtcConnection.on('dc:open', function(channel, peerId) {
-  console.log('data channel opened for peer: ' + peerId)
+    console.log('data channel opened for peer: ' + peerId)
+  
+    var stream = window.stream = rtcDataStream(channel)
+    var emitter = window.emitter = duplexEmitter(stream)
+  
+    emitter.emit('ready')
+    emitter.on('ready', function() {
+      console.log('got a ready')
+    })
 
-  var stream = window.stream = rtcDataStream(channel)
-  var emitter = window.emitter = duplexEmitter(stream)
-
-  emitter.emit('ready')
-  emitter.on('ready', function() {
-    console.log('got a ready')
   })
-
-})
