4dd00c986e4f5b6363e992c1fa50e2120130deb6
Dan Finlay <dan@danfinlay.com>
2014-10-31 08:33:01 -0700

    Linted

--- example.js
+++ example.js
@@ -3,17 +3,17 @@ var duplexEmitter = require('duplex-emitter')
 var rtcDataStream = require('./index.js')
 
 quickconnect('http://rtc.io/switchboard', { room: 'rtc-data-stream-demo' })
-  .createDataChannel('demo')
-  .on('channel:opened:demo', function(peerId, channel) {
+.createDataChannel('demo')
+.on('channel:opened:demo', function(peerId, channel) {
 
-    console.log('data channel opened for peer: ' + peerId)
-  
-    var stream = window.stream = rtcDataStream(channel)
-    var emitter = window.emitter = duplexEmitter(stream)
-  
-    emitter.emit('ready')
-    emitter.on('ready', function() {
-      console.log('got a ready')
-    })
+  console.log('data channel opened for peer: ' + peerId)
 
+  var stream = window.stream = rtcDataStream(channel)
+  var emitter = window.emitter = duplexEmitter(stream)
+
+  emitter.emit('ready')
+  emitter.on('ready', function() {
+    console.log('got a ready')
   })
+
+})
