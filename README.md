9834e45abcf0d2ba4e499c3bc5ca7d70e34ffbf2
aaron <a.aa.akio.aaron@gmail.com>
2015-04-01 01:18:25 -0700

    internals overhaul

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 # rtc-data-stream
 
-convert a reliable [RTCDataChannel](http://dev.w3.org/2011/webrtc/editor/webrtc.html#rtcdatachannel) into a stream
+convert a reliable [RTCDataChannel](http://dev.w3.org/2011/webrtc/editor/webrtc.html#rtcdatachannel) into a stream.
 
-use HTML5 [WebRTC](http://www.webrtc.org/) the node way -- with streams
+use HTML5 [WebRTC](http://www.webrtc.org/) the node way -- with streams.
 
 ## use
-
 you can use [browserify](http://github.com/substack/node-browserify) to package this module for browser use.
 
 ```javascript
@@ -15,12 +14,12 @@ var quickconnect = require('rtc-quickconnect')
 quickconnect('http://rtc.io/switchboard', { room: 'rtc-data-stream-demo' })
   .createDataChannel('chat')
   .on('channel:opened:chat', function(peerId, channel) {
-    var rtc = rtcDataStream(channel)
-    rtc.pipe(somewhereAwesome)
+    
+    rtcDataStream(channel)
+      .pipe(somewhereAwesome)
+
   })
 ```
-
-`rtc` is a stream and speaks stream events: `data`, `error` and `end`. that means you can pipe output to anything that accepts streams.
 
 ## demo
 Open [this demo](http://requirebin.com/?gist=1ac2891d276ae07e46cd) in two windows to start a chat over rtc-data-stream
@@ -43,4 +42,4 @@ npm start
 Based on [websocket-stream](https://github.com/maxogden/websocket-stream) by [max ogden](https://twitter.com/maxogden)
 
 ## license
-BSD LICENSE
+MIT LICENSE
--- index.js
+++ index.js
@@ -1,68 +1,51 @@
-var stream = require('stream')
+var DuplexStream = require('stream').Duplex
 var util = require('util')
 
-function RtcDataStream(rtcChannel, options) {
+module.exports = RtcDataStream
+
+
+util.inherits(RtcDataStream, DuplexStream)
+
+function RtcDataStream(rtcChannel) {
   if (!(this instanceof RtcDataStream)) return new RtcDataStream(rtcChannel, options)
-  stream.Stream.call(this)
-  this.options = options || {}
-  this.readable = true
-  this.writable = true
-  this._buffer = []
+
+  DuplexStream.call(this)
  
   // bind events
-  this.rtc = rtcChannel
-  this.rtc.addEventListener('message', this.onMessage.bind(this))
-  this.rtc.onerror =  this.onError.bind(this)
-  this.rtc.onclose = this.onClose.bind(this)
-  this.rtc.onopen = this.onOpen.bind(this)
-  if (this.rtc.readyState === 'open') this._open = true
+  var rtc = this.rtc = rtcChannel
+  rtc.addEventListener('message', this._onMessage.bind(this))
+  rtc.onerror =  this._onError.bind(this)
+  rtc.onclose = this._onClose.bind(this)
+  rtc.onopen = this._onOpen.bind(this)
+  if (rtc.readyState === 'open') this._open = true
+
+  // cleanup
+  this.on('finish', function(){
+    rtc.close()
+  })
+  this.on('error', function(){
+    rtc.close()
+  })
 }
 
-util.inherits(RtcDataStream, stream.Stream)
-
-module.exports = RtcDataStream
-module.exports.RtcDataStream = RtcDataStream
-
-RtcDataStream.prototype.onMessage = function(e, flags) {
-  var data = e
-  if (data.data) data = data.data
-  
-  // type must be a Typed Array (ArrayBufferView)
-  var type = this.options.type
-  if (type && data instanceof ArrayBuffer) data = new type(data)
-  
-  this.emit('data', data, flags)
+RtcDataStream.prototype._onMessage = function(event, flags) {
+  var data = event.data ? event.data : event
+  this.push(data)
 }
 
-RtcDataStream.prototype.onError = function(err) {
+RtcDataStream.prototype._onError = function(err) {
   this.emit('error', err)
 }
 
-RtcDataStream.prototype.onClose = function(err) {
-  if (this._destroy) return
-  this.emit('end')
-  this.emit('close')
+RtcDataStream.prototype._onClose = function() {
+  this.push(null)
 }
 
-RtcDataStream.prototype.onOpen = function(err) {
-  if (this._destroy) return
-  this._open = true
-  for (var i = 0; i < this._buffer.length; i++) {
-    this._write(this._buffer[i])
-  }
-  this._buffer = undefined
-  this.emit('open')
-  this.emit('connect')
-  if (this._end) this.rtc.close()
+RtcDataStream.prototype._onOpen = function(err) {
+  this.emit('readable')
 }
 
-RtcDataStream.prototype.write = function(data) {
-  if (!this._open) {
-    this._buffer.push(data)
-  } else {
-    this._write(data)
-  }
-}
+RtcDataStream.prototype._read = noop
 
 RtcDataStream.prototype._write = function(data) {
   try {
@@ -70,20 +53,11 @@ RtcDataStream.prototype._write = function(data) {
   } catch(e) {
     if (e.name == 'NetworkError') {
       // the stream closed but didn't tell us
-      this.onClose(e);
+      this._onClose(e);
     } else {
-      this.onError(e);
+      this._onError(e);
     }
   }
 }
 
-RtcDataStream.prototype.end = function(data) {
-  if (data !== undefined) this.write(data)
-  if (this._open) this.rtc.close()
-  this._end = true
-}
-
-RtcDataStream.prototype.destroy = function() {
-  this._destroy = true
-  this.rtc.close()
-}
+function noop() {}
